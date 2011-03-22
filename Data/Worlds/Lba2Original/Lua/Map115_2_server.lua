a58fcd63d007087cca40958f340531be5831b47f
Rincevent <vdelage@gmail.com>
2011-03-22 21:36:54 +0000

    fixed alpha

--- Data/Worlds/Lba2Original/Lua/Map115_2_server.lua
+++ Data/Worlds/Lba2Original/Lua/Map115_2_server.lua
@@ -17,8 +17,8 @@ function InitMap(environment)
 	Actor_1.DisplayDesc.CastShadow = false
 	Actor_1.DisplayDesc.ColorR = 1
 	Actor_1.DisplayDesc.ColorG = 1
-	Actor_1.DisplayDesc.ColorB = 2.10195e-044
-	Actor_1.DisplayDesc.ColorA = 5.03608e-024
+	Actor_1.DisplayDesc.ColorB = 1
+	Actor_1.DisplayDesc.ColorA = 1
 	Actor_1.DisplayDesc.TransX = 0
 	Actor_1.DisplayDesc.TransY = 0
 	Actor_1.DisplayDesc.TransZ = 0
