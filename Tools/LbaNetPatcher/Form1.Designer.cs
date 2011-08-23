4e8b09186351de5669cffa9835e6ffb0b4c1b5bb
Rincevent <vdelage@gmail.com>
2011-08-23 20:34:18 +0000

    removed arena

---
+++ Tools/LbaNetPatcher/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace LbaNetPatcher
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(832, 496);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://www.lbanet.net/patcher.php", System.UriKind.Absolute);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 543);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(808, 27);
+            this.progressBar.TabIndex = 1;
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Enabled = false;
+            this.buttonPlay.Location = new System.Drawing.Point(277, 589);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(102, 29);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonExit.Location = new System.Drawing.Point(394, 589);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(102, 29);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(9, 518);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(301, 13);
+            this.labelInfo.TabIndex = 4;
+            this.labelInfo.Text = "Checking the server for update. This can take a few minutes...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainWindow
+            // 
+            this.AcceptButton = this.buttonPlay;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(832, 630);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lbanet Patcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tools/LbaNetPatcher/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LbaNetPatcher
+{
+    public partial class MainWindow : Form
+    {
+        private System.Diagnostics.Process _startedprocess;
+
+        public MainWindow()
+        {
+            _startedprocess = null;
+
+            InitializeComponent();
+
+            try
+            {
+                System.IO.StreamReader ipfile = new System.IO.StreamReader("patchip.cfg");
+                string ipString = ipfile.ReadToEnd();
+                ipfile.Close();
+
+                System.Diagnostics.ProcessStartInfo psi =
+                    new System.Diagnostics.ProcessStartInfo(@"patchingclient");
+                psi.Arguments = " --IcePatch2.Remove=0 --IcePatch2.Endpoints=\"tcp -h "+ipString+" -p 10000\" .";
+                psi.CreateNoWindow = true;
+                psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                psi.UseShellExecute = true;
+                _startedprocess = System.Diagnostics.Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                labelInfo.Text = "Error during patching: " + ex.Message;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar.Increment(1);
+            if (progressBar.Value == progressBar.Maximum)
+                progressBar.Value = 0;
+
+            if (_startedprocess != null && _startedprocess.HasExited)
+            {
+                if (_startedprocess.ExitCode == 0)
+                {
+                    labelInfo.Text = "Game updated succesfully.";
+                    buttonPlay.Enabled = true;
+                    progressBar.Value = progressBar.Maximum;
+                }
+                else
+                {
+                    labelInfo.Text = "Error during patching. Please check the file Patching.log for details.";
+                    progressBar.Value = 0;
+                }
+
+                timer1.Enabled = false;
+            }
+        }
+
+
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"LBANetGame.exe");
+            Application.Exit();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!_startedprocess.HasExited)
+                {
+                    _startedprocess.Kill();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
