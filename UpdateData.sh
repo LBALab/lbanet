a61ece0286774b6a6d6c55e22dd9c116824c7465
Rincevent <vdelage@gmail.com>
2012-04-22 16:09:39 +0000

    fixed script end

--- UpdateData.sh
+++ UpdateData.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 
 echo Updating Data directory...
-svn update Data
-svn export --force Data/ Patch/Lbanet/Data/
-cp -rf Data Servers/IceGridServer
-cp *.HQR Servers/IceGridServer/Data/Worlds/Lba1Original/Models 
+svn update ~/neolbanet/Data
+svn export --force ~/neolbanet/Data/ ~/neolbanet/Patch/Lbanet/Data/
+cp -rf ~/neolbanet/Data ~/neolbanet/Servers/IceGridServer
+
+cp ~/neolbanet/*.HQR ~/neolbanet/Servers/IceGridServer/Data/Worlds/Lba1Original/Models 
 
 
 echo Updating Colozen directory...
-svn update tmpsvn/Colozen
-svn export --force tmpsvn/Colozen Patch/Lbanet/Data/Worlds/Colozen
-cp -rf tmpsvn/Colozen Servers/IceGridServer/Data/Worlds
+svn update ~/neolbanet/tmpsvn/Colozen
+svn export --force ~/neolbanet/tmpsvn/Colozen ~/neolbanet/Patch/Lbanet/Data/Worlds/Colozen
+cp -rf ~/neolbanet/tmpsvn/Colozen ~/neolbanet/Servers/IceGridServer/Data/Worlds
 
 
 echo Updating Lba1Expanded directory...
-svn update tmpsvn/Lba1Expanded
-svn export --force tmpsvn/Lba1Expanded Patch/Lbanet/Data/Worlds/Lba1Expanded
-cp -rf tmpsvn/Lba1Expanded Servers/IceGridServer/Data/Worlds
+svn update ~/neolbanet/tmpsvn/Lba1Expanded
+svn export --force ~/neolbanet/tmpsvn/Lba1Expanded ~/neolbanet/Patch/Lbanet/Data/Worlds/Lba1Expanded
+cp -rf ~/neolbanet/tmpsvn/Lba1Expanded ~/neolbanet/Servers/IceGridServer/Data/Worlds
 
 
 echo Updating Lba1Original directory...
-svn update tmpsvn/Lba1Original
-svn export --force tmpsvn/Lba1Original Patch/Lbanet/Data/Worlds/Lba1Original
-cp -rf tmpsvn/Lba1Original Servers/IceGridServer/Data/Worlds
+svn update ~/neolbanet/tmpsvn/Lba1Original
+svn export --force ~/neolbanet/tmpsvn/Lba1Original ~/neolbanet/Patch/Lbanet/Data/Worlds/Lba1Original
+cp -rf ~/neolbanet/tmpsvn/Lba1Original ~/neolbanet/Servers/IceGridServer/Data/Worlds
 
 
 echo Updating Lba2Original directory...
-svn update tmpsvn/Lba2Original
-svn export --force tmpsvn/Lba2Original Patch/Lbanet/Data/Worlds/Lba2Original
-cp -rf tmpsvn/Lba1ExpLba2Original Servers/IceGridServer/Data/Worlds
+svn update ~/neolbanet/tmpsvn/Lba2Original
+svn export --force ~/neolbanet/tmpsvn/Lba2Original ~/neolbanet/Patch/Lbanet/Data/Worlds/Lba2Original
+cp -rf ~/neolbanet/tmpsvn/Lba2Original ~/neolbanet/Servers/IceGridServer/Data/Worlds
 
